7121d565470fd6d1756274bb31f3a95beb1463e7
unknown <mnnlthmpsn@outlook.com>
2020-04-15 15:32:56 -0700

    15th april, 2020

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -18,14 +18,14 @@ const Explore = () => {
                             </div>
                             <div class="row">
                                 <div class="col-8">
-                                    <h4 style={{ color: "black !important" }}><a href="#" style={{ color: "black" }}>Artificial Intelligence</a></h4>
+                                    <h4 style={{ color: "black !important" }}><a href="#" style={{ color: "black" }}>Business Administration</a></h4>
                                 </div>
                                 <div class="col-4"><a href="#" class="small-text"></a></div>
                             </div>
                             <div class="product-rating"><a href="#" class="small-text">15 topics</a></div>
                             <div class="row">
                                 <div class="col-12">
-                                    <p class="product-description">In computer science, artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, in contrast to the natural intelligence displayed by humans and animals. Leading AI textbooks define the field as the study of "intelligent agents": any device that perceives its environment and takes actions that maximize its chance of successfully achieving its goals</p>
+                                    <p class="product-description">Business administration is the administration of a business. It includes all aspects of overseeing and supervising business operations. From the point of view of management and leadership, it also covers fields that include accounting, finance, project management and marketing</p>
                                     <div class="row">
                                         <Link to='/catalog'><div class="col-6"><button class="btn btn-light" type="button" style={{ backgroundColor: "rgb(40,167,69)" }}>Enroll</button></div></Link>
                                     </div>
@@ -41,14 +41,14 @@ const Explore = () => {
                             </div>
                             <div class="row">
                                 <div class="col-8">
-                                    <h4 style={{ color: "black !important" }}><a href="#" style={{ color: "black" }}>Artificial Intelligence</a></h4>
+                                    <h4 style={{ color: "black !important" }}><a href="#" style={{ color: "black" }}>Computer Science</a></h4>
                                 </div>
                                 <div class="col-4"><a href="#" class="small-text"></a></div>
                             </div>
                             <div class="product-rating"><a href="#" class="small-text">15 topics</a></div>
                             <div class="row">
                                 <div class="col-12">
-                                    <p class="product-description">In computer science, artificial intelligence (AI), sometimes called machine intelligence, is intelligence demonstrated by machines, in contrast to the natural intelligence displayed by humans and animals. Leading AI textbooks define the field as the study of "intelligent agents": any device that perceives its environment and takes actions that maximize its chance of successfully achieving its goals</p>
+                                    <p class="product-description">Computer science is the study of processes that interact with data and that can be represented as data in the form of programs. It enables the use of algorithms to manipulate, store, and communicate digital information</p>
                                     <div class="row">
                                         <Link to='/catalog'><div class="col-6"><button class="btn btn-light" type="button" style={{ backgroundColor: "rgb(40,167,69)" }}>Enroll</button></div></Link>
                                     </div>
